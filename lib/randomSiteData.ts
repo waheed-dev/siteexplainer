b47c9d31d801ccd2d8f6ccd358d23949febd2353
Waheed <a.waheed3738@gmail.com>
2023-02-08 09:15:00 +0500

    added random site generation and new layout

---
+++ lib/randomSiteData.ts
@@ -0,0 +1,11 @@
+export const randomSiteData: string[] = ['https://blogger.com', 'https://google.com', 'https://youtube.com',
+    'https://mozilla.org', 'https://cloudflare.com', 'https://maps.google.com','https://docs.google.com',
+    'https://whatsapp.com',"https://drive.google.com",'https://drive.google.com','https://github.com',
+    'https://vk.com','https://facebook.com','https://vimeo.com','https://amazon.com',
+    'https://forbes.com','https://dropbox.com','https://bbc.co.uk','https://cnn.com','https://medium.com',
+    'https://slideshare.net','https://nytimes.com',
+    'https://shopify.com','https://es.wikipedia.org','https://paypal.com','https://theguardian.com',
+    'https://w3.org','https://telegraph.co.uk','https://thesun.co.uk',
+
+
+]
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
+  darkMode: "class",
   future: {
     hoverOnlyWhenSupported: true,
   },
@@ -9,7 +11,61 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: ["0.75rem", { lineHeight: "1rem" }],
+      sm: ["0.875rem", { lineHeight: "1.5rem" }],
+      base: ["1rem", { lineHeight: "1.75rem" }],
+      lg: ["1.125rem", { lineHeight: "2rem" }],
+      xl: ["1.25rem", { lineHeight: "2rem" }],
+      "2xl": ["1.5rem", { lineHeight: "2rem" }],
+      "3xl": ["2rem", { lineHeight: "2.5rem" }],
+      "4xl": ["2.5rem", { lineHeight: "3.5rem" }],
+      "5xl": ["3rem", { lineHeight: "3.5rem" }],
+      "6xl": ["3.75rem", { lineHeight: "1" }],
+      "7xl": ["4.5rem", { lineHeight: "1.1" }],
+      "8xl": ["6rem", { lineHeight: "1" }],
+      "9xl": ["8rem", { lineHeight: "1" }],
+    },
+    extend: {
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s linear forwards",
+        marquee: "marquee var(--marquee-duration) linear infinite",
+        "spin-slow": "spin 4s linear infinite",
+        "spin-slower": "spin 6s linear infinite",
+        "spin-reverse": "spin-reverse 1s linear infinite",
+        "spin-reverse-slow": "spin-reverse 4s linear infinite",
+        "spin-reverse-slower": "spin-reverse 6s linear infinite",
+      },
+      keyframes: {
+        "fade-in": {
+          from: {
+            opacity: 0,
+          },
+          to: {
+            opacity: 1,
+          },
+        },
+        marquee: {
+          "100%": {
+            transform: "translateY(-50%)",
+          },
+        },
+        "spin-reverse": {
+          to: {
+            transform: "rotate(-360deg)",
+          },
+        },
+      },
+
+      maxWidth: {
+        "2xl": "40rem",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms"), require("@headlessui/tailwindcss")],
+  darkMode: "class",
 };
+

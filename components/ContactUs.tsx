88a2404501f4263c9add668d8b4dc07bf452192c
MDaud32 <theprofessor543@gmail.com>
2023-02-10 09:24:06 +0500

    first  commit

---
+++ components/ContactUs.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React, { useState } from "react";
+
+const ContactUs: React.FC = () => {
+  let [open, setOpen] = useState(false);
+  return (
+    <>
+      <button
+        onClick={() => setOpen(false)}
+        className="text-white font-semibold text-xl hover:underline dark:text-black md:text-gray-700 md:dark:text-white">
+        <Link href="https://docs.google.com/forms/d/e/1FAIpQLSfUa3qfg05zRupFY07D2AAEmw3it_Pfs3uc6su4gtkuP2UE4g/viewform">
+          Contact Us
+        </Link>
+      </button>
+    </>
+  );
+};
+export default ContactUs;

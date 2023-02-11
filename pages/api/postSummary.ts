4a0b6450706b67507a61d9bf11267827b109becb
ArthurDev <arthurdevoficial@gmail.com>
2023-02-11 11:54:44 -0300

    Now it's pushing the summaries to the database and getting them if they have already been generated

--- pages/api/postSummary.ts
+++ pages/api/postSummary.ts
@@ -1,13 +1,13 @@
 import { prisma } from '../../lib/prisma'
 
 export default async function handle(req: any, res: any) {
-    const { url, summary} = req.body;
-  
-    const result = await prisma.summary.create({
-      data: {
-        url: url,
-        summary: summary
-      },
-    });
-    res.json(result);
-  }
+  const { url, summary } = req.body;
+
+  const result = await prisma.summary.create({
+    data: {
+      url: url,
+      summary: summary
+    },
+  });
+  res.json(result);
+}
